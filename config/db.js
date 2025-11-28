a5c933526bbf56eab3c5472573104a803332602b
jatin <jatinkumar20056@gmail.com>
2025-11-28 22:34:04 +0530

    fix db

--- config/db.js
+++ config/db.js
@@ -1,20 +1,16 @@
-// config/db.js
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.set("strictQuery", true); // fixes the deprecation warning
+mongoose.set("strictQuery", true);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
-    process.exit(1);
+    // Do NOT exit in Render (it restarts forever)
   }
 };
 
